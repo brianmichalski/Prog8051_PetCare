3accdcd5eadc7ab2c1ff4486a6c0686a6c438d94
brianmichalski <90117970+brianmichalski@users.noreply.github.com>
2024-05-30 20:18:59 -0400

    add: test cases for creating animal

--- PetManager.Tests/AddPetTests.cs
+++ PetManager.Tests/AddPetTests.cs
@@ -1,20 +1,48 @@
+using PetManager.Core.Model.Implementation;
+using PetManager.Core.Model.Type;
 using PetManager.Service;
 
 namespace PetManager.Tests;
 
 public class AddPetTests
 {
-    PetService petService;
+    PetService PetService;
 
     [SetUp]
     public void Setup()
     {
-        this.petService = PetService.GetInstance();
+        this.PetService = PetService.GetInstance();
     }
 
     [Test]
-    public void ListPetTypesPass()
+    [TestCase(SpecieEnum.Cat, "Meow")]
+    public void CreateAnimalCheckNotNull_Pass(SpecieEnum specie, string name)
     {
-        Assert.Pass();
+        var animal = this.PetService.CreateAnimal(specie, name);
+        Assert.That(animal, Is.Not.Null);
+    }
+
+    [Test]
+    [TestCase(SpecieEnum.Cat, "Meow", ExpectedResult = typeof(Cat))]
+    [TestCase(SpecieEnum.Dog, "Snoopy", ExpectedResult = typeof(Dog))]
+    [TestCase(SpecieEnum.GuineaPig, "Porky", ExpectedResult = typeof(GuineaPig))]
+    [TestCase(SpecieEnum.Turtle, "Speed", ExpectedResult = typeof(Turtle))]
+    public Type CreateAnimalCheckTypes_Pass(SpecieEnum specie, string name)
+    {
+        var animal = this.PetService.CreateAnimal(specie, name);
+
+        return animal.GetType();
+    }
+
+    [Test]
+    [TestCase(SpecieEnum.Cat, "Meow", ExpectedResult = "Meow")]
+    [TestCase(SpecieEnum.Dog, "Snoopy", ExpectedResult = "Snoopy")]
+    [TestCase(SpecieEnum.GuineaPig, "Porky", ExpectedResult = "Porky")]
+    [TestCase(SpecieEnum.Turtle, "Speed", ExpectedResult = "Speed")]
+    public string CreateAnimalCheckName(SpecieEnum specie, string name)
+    {
+        var animal = this.PetService.CreateAnimal(specie, name);
+
+        return animal.Name;
     }
 }
